71eefc5dd5700ddfdadb604a0035e3376e600323
Glitch (hello-sqlite) <none>
2019-04-02 08:25:25 +0000

    🏙🏔 Checkpoint ./views/post_it.html:3115107/1640

--- seeds/sending.js
+++ seeds/sending.js
@@ -5,9 +5,10 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        { login: 'rowValue1'},
+        {txt: 'rowValue2'},
+        {coor: 'rowValue3'}
       ]);
-    });
+    );
+
 };
