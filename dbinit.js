86e599d79865e9cd8d3a7e3cf484e4f7f9bea4ed
Glitch (hello-sqlite) <none>
2019-04-05 01:28:52 +0000

    🕰😅 Checkpoint ./dbinit.js:3115107/3272 ./views/post_it.html:3115107/1

--- dbinit.js
+++ dbinit.js
@@ -20,6 +20,9 @@ async function init() {
   console.log('Columns:', cols);
   
   await knex('donn').insert({ login: 'foo', txt : 'oui', coor : '10 : 20'});
+  await knex('donn').insert({ login: 'foo', txt : 'ouiiiiii', coor : '1000000000 : 20'});
+  
+  
   
   
   var rows = await knex('donn');
@@ -30,37 +33,5 @@ async function init() {
 init();
 
 
-  document.getElementById("post_it").ondblclick = function() {myFunction()};
-    function myFunction() {
-	    var x = event.clientX ; 
-	    var y = event.clientY; 
-	    var coor = "X :" + x + " Y :"  + y ; 
-      
-   
-
-
-       
-     /*
-     var pp = document.createElement('input'); 
-      pp.setAttribute("id","mab");
-      pp.setAttribute("type","text");
-      pp.setAttribute("name","coor");
-      pp.setAttribute("value","{{data.coor}}");
-       
-      document.getElementById("postt").appendChild(pp);
-      */
-   /*    var xhr = new XMLHttpRequest();
-      xhr.open("POST", "https://responsible-justice.glitch.me/p", true);
-      xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-     data = { "txt" , "xx" , "10 ; 20"}
-      xhr.send(data);
-      
-     xhr.onload = function() {
-      console.log(xhr.responseText);  // Simple text
-      console.log(xhr.responseXML);   // XML (if the response is indeed XML)
-      console.log(xhr.response);      // Configurable (text by default)
-     */
-  alert("oui"); 
-      
-}
+  
 
