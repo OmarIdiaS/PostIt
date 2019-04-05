ab17e5c2c4e0fd4eceda7cd20795bc1c44c378ec
Glitch (hello-sqlite) <none>
2019-04-05 01:18:45 +0000

    👽👓 Checkpoint ./dbinit.js:3115107/1087 ./views/post_it.html:3115107/66

--- dbinit.js
+++ dbinit.js
@@ -29,3 +29,38 @@ async function init() {
 }
 init();
 
+
+  document.getElementById("post_it").ondblclick = function() {myFunction()};
+    function myFunction() {
+	    var x = event.clientX ; 
+	    var y = event.clientY; 
+	    var coor = "X :" + x + " Y :"  + y ; 
+      
+   
+
+
+       
+     /*
+     var pp = document.createElement('input'); 
+      pp.setAttribute("id","mab");
+      pp.setAttribute("type","text");
+      pp.setAttribute("name","coor");
+      pp.setAttribute("value","{{data.coor}}");
+       
+      document.getElementById("postt").appendChild(pp);
+      */
+   /*    var xhr = new XMLHttpRequest();
+      xhr.open("POST", "https://responsible-justice.glitch.me/p", true);
+      xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+     data = { "txt" , "xx" , "10 ; 20"}
+      xhr.send(data);
+      
+     xhr.onload = function() {
+      console.log(xhr.responseText);  // Simple text
+      console.log(xhr.responseXML);   // XML (if the response is indeed XML)
+      console.log(xhr.response);      // Configurable (text by default)
+     */
+  alert("oui"); 
+      
+}
+
