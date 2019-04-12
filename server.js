c3c523578c41c53c57e128e52a9d47943b5bdf62
Glitch (responsible-justice) <none>
2019-04-12 11:04:06 +0000

    📠🏗 Checkpoint ./server.js:3115107/12

--- server.js
+++ server.js
@@ -191,7 +191,7 @@ app.get('/logout', (req, res) => {
 app.get('/:n', async (req, res) => {
 res.render('tabuser.html', { 
    
-        donn: await knex.raw(`SELECT * FROM donn WHERE useer = n`),
+        donn: await knex.raw(`SELECT * FROM donn WHERE useer = $n`),
         current: req.session.user,
       });
 });
