408c6acf883cb45fc00e746708f5d09e7e887fb4
Glitch (hello-sqlite) <none>
2019-04-12 09:53:45 +0000

    🐞⛷ Checkpoint ./server.js:3115107/196

--- server.js
+++ server.js
@@ -190,7 +190,7 @@ app.get('/logout', (req, res) => {
 
 app.get('/:n', async (req, res) => {
 res.render('tabuser.html', { 
-        donn: await knex.raw(`SELECT * FROM donn where useer='$n'`),
+        donn: await knex.raw(`SELECT * FROM donn where useer=$n`),
         current: req.session.user,
       });
 });
